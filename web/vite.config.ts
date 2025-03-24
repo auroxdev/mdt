1b17acefc56b8104602de1e207e700c3393a0480
Matthew <22198949+mafewtm@users.noreply.github.com>
2025-03-24 11:33:08 -0400

    fix(web): update to tw4

--- web/vite.config.ts
+++ web/vite.config.ts
@@ -1,10 +1,12 @@
 import path from 'path';
 import { defineConfig } from 'vite';
-import react from '@vitejs/plugin-react';
+import tailwindcss from '@tailwindcss/vite'
 
 // https://vitejs.dev/config/
 export default defineConfig({
-  plugins: [react()],
+  plugins: [
+    tailwindcss(),
+  ],
   base: './',
   publicDir: false,
   build: {
@@ -12,7 +14,7 @@ export default defineConfig({
   },
   resolve: {
     alias: {
-      "@": path.resolve(__dirname, "./src"),
+      '@': path.resolve(__dirname, './src'),
     },
   },
 });
