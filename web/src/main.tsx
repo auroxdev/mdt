70ac97c355bfe003c06358bcd1b4fccd0b11e6e1
Matthew <22198949+mafewtm@users.noreply.github.com>
2025-03-24 12:29:06 -0400

    fix(web/main): streamline imports

--- web/src/main.tsx
+++ web/src/main.tsx
@@ -1,32 +1,25 @@
-import React from 'react';
-import ReactDOM from 'react-dom/client';
+import { StrictMode } from 'react';
+import { createRoot } from 'react-dom/client';
 import { BrowserRouter } from 'react-router';
-import { debugData } from './utils/debugData';
 import { isDevBrowser } from '@/utils/isDevBrowser';
 import App from './App';
 import './main.css';
-
-debugData([
-  {
-    action: 'setVisible',
-    data: true,
-  },
-]);
 
 if (isDevBrowser()) {
   const root = document.getElementById('root');
 
   // https://i.imgur.com/iPTAdYV.png - Night time img
-  root!.style.backgroundImage = 'url("https://i.imgur.com/3pzRj9n.png")';
+  root!.style.backgroundImage = `url('https://i.imgur.com/3pzRj9n.png')`;
   root!.style.backgroundSize = 'cover';
   root!.style.backgroundRepeat = 'no-repeat';
   root!.style.backgroundPosition = 'center';
 }
 
-ReactDOM.createRoot(document.getElementById('root')!).render(
-  <React.StrictMode>
+const root = createRoot(document.getElementById('root')!);
+root.render(
+  <StrictMode>
     <BrowserRouter>
       <App />
     </BrowserRouter>
-  </React.StrictMode>
+  </StrictMode>
 );
