635c6d8070acf29a6962cc237e3f6e634290794d
Matthew <22198949+mafewtm@users.noreply.github.com>
2025-03-27 12:15:41 -0400

    feat(web/mdt): profile and vehicle layout

--- web/src/layouts/mdt/pages/vehicles/Vehicles.tsx
+++ web/src/layouts/mdt/pages/vehicles/Vehicles.tsx
@@ -1,7 +1,19 @@
+import { Search } from '@carbon/icons-react';
+
 export default function Vehicles() {
   return (
-    <div className='flex w-full items-center justify-center rounded-md border border-zinc-650 bg-zinc-750 drop-shadow-md gap-5'>
-      <h1 className='text-zinc-300'>Vehicles</h1>
+    <div className='flex flex-col size-full font-robotoflex text-zinc-300 justify-between gap-5'>
+      <div className='flex h-10 w-full rounded-md border border-zinc-650 bg-zinc-750 drop-shadow-md p-3'>
+        <div className='flex flex-row gap-2'>
+          <Search className='text-zinc-450' />
+          <p className='font-semibold text-zinc-450 leading-none'>SEARCH</p>
+        </div>
+      </div>
+      <div className='flex size-full rounded-md border border-zinc-650 bg-zinc-750 drop-shadow-md p-3'>
+        <div className='flex flex-col gap-2'>
+          <p className='font-semibold text-zinc-450 leading-none'>VEHICLE</p>
+        </div>
+      </div>
     </div>
   );
 }
