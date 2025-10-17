e3469f40224d8d7155b55d6d2370a54dc853dd09
Matthew <22198949+mafewtm@users.noreply.github.com>
2025-10-17 03:14:16 -0400

    fix(web): other build issues

--- web/src/components/ui/dropdown-menu.tsx
+++ web/src/components/ui/dropdown-menu.tsx
@@ -4,7 +4,7 @@ import * as React from "react"
 import * as DropdownMenuPrimitive from "@radix-ui/react-dropdown-menu"
 import { Checkmark, ChevronRight, CircleOutline } from "@carbon/icons-react";
 
-import { cn } from "@/lib/utils"
+import { cn } from "../../lib/utils"
 
 function DropdownMenu({
   ...props
